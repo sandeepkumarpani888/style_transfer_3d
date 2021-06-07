603e7a28801d713759ba42bd53445bbdf3737c2b
sandeep <sandeepkumarpani888@gmail.com>
2021-06-07 05:23:10 +0100

    changes to script

--- examples/run.sh
+++ examples/run.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
 DIR=./examples/data
-python ./examples/run.py -im ${DIR}/meshes/teapot.obj -is ${DIR}/styles/gris1.jpg -o ${DIR}/results/teapot_gris.gif -lc 2000000000 -ltv 10000
-python ./examples/run.py -im ${DIR}/meshes/bunny.obj -is ${DIR}/styles/munch1.jpg -o ${DIR}/results/bunny_munch.gif -lc 2000000000 -ltv 100000
+
+# To test out with your own examples here are the steps:
+# Put your file in /examples/data under style_transfer_3d
+# Lets say the obj file which you put is called architecture.obj
+# The change: {ME} in the line below everywhere with architecture
+# The generated gif will be avilable as /examples/data/architeture.gif
+########################
+python ./examples/run.py -im ${DIR}/house_10.obj -is ${DIR}/styles/gris1.jpg -o ${DIR}/house_10.gif -lc 2000000000 -ltv 10000
+########################
+# python ./examples/run.py -im ${DIR}/meshes/teapot.obj -is ${DIR}/styles/gris1.jpg -o ${DIR}/results/teapot_gris.gif -lc 2000000000 -ltv 10000
+# python ./examples/run.py -im ${DIR}/meshes/bunny.obj -is ${DIR}/styles/munch1.jpg -o ${DIR}/results/bunny_munch.gif -lc 2000000000 -ltv 100000
